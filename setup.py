6221094e1eee0759c53c8c9d65d8c48d27260593
Gabriel T <gab.toledo@duocuc.cl>
2026-05-13 20:22:57 -0400

    fix/borradores - consumo scripts

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
+# indica en linux y macOS que este archivo se debe ejecutar con Python
 from setuptools import setup, find_packages
 
 # configuramos la carpeta scripts como un paquete de Python
 setup(
     name="scripts",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    description="Paquete local con funciones de preprocesamiento y modelado para ciencia de datos",
+    install_requires=[
+        "pandas>=2.0.0",
+        "numpy>=1.24.0",
+        "scikit-learn",
+    ]
 )
